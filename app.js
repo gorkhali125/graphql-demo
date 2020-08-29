53c9b3cab5d57bca65b19587cb2fcb55da681717
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-08-29 12:45:11 +0545

    Add main app

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/', (req, res) => {
+	res.status(200).json({ message: 'Server Connected!' });
+});
+
+
+const mongoUri = 'mongodb://localhost:27017/graphql-demo';
+mongoose
+	.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true, keepAlive: 1 })
+	.then(() => {
+		app.listen(3002, () => {
+			console.log('Server listening to port 3002...');
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
