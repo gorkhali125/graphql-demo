f15b82895eb0671712dc516029901b16721219ca
Deepesh Khanal <deepeshkhanal@gmail.com>
2020-08-30 13:08:11 +0545

    Add graphql route

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 const Note = require('./models/note');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/', (req, res) => {
-	res.status(200).json({ message: 'Server Connected!' });
-});
 
+app.use(
+	'/graphql',
+	graphqlHttp({
+		graphiql: true
+	})
+);
+
+app.use('/', (req, res) => {
+	res.status(200).json({ message: 'Server Connected! For trying out graphql, goto http://localhost:3002/graphql' });
+});
 
 const mongoUri = 'mongodb://localhost:27017/graphql-demo';
 mongoose
