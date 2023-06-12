ec4cb1725080cc095501408d049a34481864c2c6
gorkhali125 <deepeshkhanal@gmail.com>
2023-06-12 23:26:45 +0545

    Add a variable for app port

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const { buildSchema } = require('graphql');
 
 const Note = require('./models/note');
 
-const app = express();
+const appPort = 3002;
 
+const app = express();
 app.use(bodyParser.json());
 
 app.use(
@@ -77,15 +78,15 @@ app.use(
 );
 
 app.use('/', (req, res) => {
-	res.status(200).json({ message: 'Server Connected! For trying out graphql, goto http://localhost:3002/graphql' });
+	res.status(200).json({ message: `Server Connected! For trying out graphql, goto http://localhost:${appPort}/graphql` });
 });
 
 const mongoUri = 'mongodb://localhost:27017/graphql-demo';
 mongoose
 	.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true, keepAlive: 1 })
 	.then(() => {
-		app.listen(3002, () => {
-			console.log('Server listening to port 3002...');
+		app.listen(appPort, () => {
+			console.log(`Server listening to port ${appPort}...`);
 		});
 	})
 	.catch((err) => {
